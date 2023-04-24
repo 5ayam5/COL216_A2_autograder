a1d89ff116ab575ccda0a3823726d4784959fa3b
chiragmohapatra <chirag131020@gmail.com>
2023-04-24 19:22:29 +0530

    testcases & outputs

--- Testcases/pipeline/private_test1.asm
+++ Testcases/pipeline/private_test1.asm
@@ -1,5 +1,5 @@
-addi $t1, $0, 5
-addi $t2, $0, 10
+addi $t1, $0, 8
+addi $t2, $0, 12
 sw $t1, 0($t2)
 lw $t3, 0($t2)
 add $t4, $t3, $t3
--- Testcases/pipeline/private_test2.asm
+++ Testcases/pipeline/private_test2.asm
@@ -1,8 +1,8 @@
-addi $t1, $0, 10
-addi $t2, $0, 20
+addi $t1, $0, 4
+addi $t2, $0, 8
 add $t3, $t1, $t2
 sw $t3, 0($t1)
-addi $t4, $0, 30
+addi $t4, $0, 12
 lw $t5, 0($t1)
 add $t6, $t5, $t4
 add $t7, $t6, $t3
