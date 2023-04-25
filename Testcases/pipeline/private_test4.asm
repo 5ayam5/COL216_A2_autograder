cbf7f259ef5f2dc2f2524637d175ee850f5f0b7e
Sayam Sethi <sayam.sethi2@gmail.com>
2023-04-25 11:44:05 +0530

    fixed private_test4.asm

--- Testcases/pipeline/private_test4.asm
+++ Testcases/pipeline/private_test4.asm
@@ -3,12 +3,12 @@ addi $t2, $0, 3
 addi $t3, $0, 0
 addi $t4, $0, 3
 addi $t5, $0, 0
-outer_loop:
+outerloop:
 addi $t3, $0, 0
-inner_loop:
+innerloop:
 add $t5, $t5, $t1
 add $t5, $t5, $t3
 addi $t3, $t3, 1
-bne $t3, $t4, inner_loop
+bne $t3, $t4, innerloop
 addi $t1, $t1, 1
-bne $t1, $t2, outer_loop
+bne $t1, $t2, outerloop
